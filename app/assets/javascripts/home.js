3bdc1c84e6e606552e4f4f94de8e7f8a04650024
CorainChicago <chaysmagan@gmail.com>
2016-07-22 09:50:28 -0500

    move header into navbar on scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
       $('.home h1').addClass('first_position');
     }
     if ($(window).scrollTop() > 281) {
-      $('.home h1').removeClass('first_position');
-      $('.home h1').addClass('second_position');
+      $('.home h1').remove();
+      console.log($( ".navbar-brand" ).length);
+      if( $( ".navbar-brand" ).length == 0 ){
+        $('ul').append("<a class='navbar-brand' href='#''>Drinking With My Cat</a>");
+      }
     }
     
   });
