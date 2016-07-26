0eb1aeaf14d4926ec01da0e1af2abc4e21479635
CorainChicago <chaysmagan@gmail.com>
2016-07-25 22:19:44 -0500

    add validation tests for Charlie model

--- spec/models/charlie_spec.rb
+++ spec/models/charlie_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Charlie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validation" do
+
+     it "valid with all requires attributes" do
+      charlie = Charlie.create(description: "Test", email: "test@test.com", image: File.open("#{Rails.root}/public/intensecharliecopy.jpg"))
+      expect(charlie.errors.count).to eq 0
+    end 
+
+     it "invalid when the email address lacks a '@' symbol" do
+      charlie = Charlie.create(description: "Test", email: "testattest.com", image: File.open("#{Rails.root}/public/intensecharliecopy.jpg"))
+      expect(charlie.errors.count).to eq 1
+    end
+
+    it "requires an email to be valid and formatted correctly" do
+      charlie = Charlie.create(description: "Test", image: File.open("#{Rails.root}/public/intensecharliecopy.jpg"))
+      expect(charlie.errors.count).to eq 2
+    end 
+  end
 end
