b2e58a51c88eaf0c47d719ce3472e904b2db9956
CorainChicago <chaysmagan@gmail.com>
2016-08-06 13:10:48 -0500

    setup a has_many relationship between list and list items

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,6 @@ class HomesController < ApplicationController
 
   def index
     @charlie = Charlie.order("created_at DESC").first(4)
+    @list = ListOfImportance.includes(:items).first
   end
 end
--- app/models/list_of_importance.rb
+++ app/models/list_of_importance.rb
@@ -1,2 +1,3 @@
 class ListOfImportance < ActiveRecord::Base
+  has_many :items
 end
