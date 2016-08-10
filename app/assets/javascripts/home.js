7b45f497007a7fe3a037da0e1efecdf97b1ad4a0
CorainChicago <chaysmagan@gmail.com>
2016-08-09 21:13:24 -0500

    add background color to list of importance

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       $('.home h1').remove();
       console.log($( ".navbar-brand" ).length);
       if( $( ".navbar-brand" ).length == 0 ){
-        $('ul').append("<a class='navbar-brand' href='#''>Drinking With My Cat</a>");
+        $('ul.topnav').append("<a class='navbar-brand' href='#''>Drinking With My Cat</a>");
       }
     }
     if ($(window).scrollTop() > 500) { 
